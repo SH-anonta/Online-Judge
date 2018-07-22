86d0b85183c8b6685e9f1fa699920fc0c80f32bf
Anonta <SH-anonta@users.noreply.github.com>
2018-07-22 21:02:03 +0600

    Add two classes: DataRepository DataValidator

---
+++ OnlineJudge/Repository/DataRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineJudge.Models;
+
+namespace OnlineJudge.Repository {
+    public partial class DataRepository {
+        private static OjDBContext getContext(){
+            return new OjDBContext();
+        }
+    }
+}
