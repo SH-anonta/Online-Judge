a8631d0b3c590400760802aee14c7a0eb98760fd
Anonta <SH-anonta@users.noreply.github.com>
2018-07-30 20:16:00 +0600

    Add new class library project for writing tests Install xunit package in new project Wrtie simple test for AnnouncementFormData validation

---
+++ OnlineJudgeTests/FormDataTests/Announcement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineJudge.FormModels;
+using Xunit;
+
+namespace OnlineJudgeTests{
+    public class AnnouncementFormDataTests{
+        [Fact]
+        public void AnnounementFormData_IsValid_ForValidData(){
+            var data = new AnnouncementFormData()
+            {
+                Title = "Hello there",
+                Description = "Testing 1 2 3"
+            };
+
+
+            FormDataValidationResult result = data.Validate();
+
+            // data should be considered valid 
+            Assert.True(result.IsValid);
+
+            // no error message should be set
+            Assert.Empty(result.ErrorMessages);
+        }
+    }
+}
