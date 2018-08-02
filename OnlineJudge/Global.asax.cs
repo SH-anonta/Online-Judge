53c8eee6a5d0e4e79f2a516bf430b1d890c35979
Manosh Talukder <manosh.office015@gmail.com>
2018-08-03 01:05:44 +0600

    Configured OnlineJudge for logging log4net log messages into localhost DB.

--- OnlineJudge/Global.asax.cs
+++ OnlineJudge/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using log4net;
 
 namespace OnlineJudge {
     public class MvcApplication : System.Web.HttpApplication {
@@ -16,6 +17,13 @@ namespace OnlineJudge {
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            //Configures the log4net configurator to launch with application.
+            log4net.Config.XmlConfigurator.Configure();
+            //We will use a Debug message when the application starts.
+            var log = LogManager.GetLogger("logger");
+            log.Debug("Application Started");
         }
     }
 }
