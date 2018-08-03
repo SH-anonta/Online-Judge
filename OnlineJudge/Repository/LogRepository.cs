bfda00ae8d4b731e43c9f738c32f84fd84840aca
Anonta <SH-anonta@users.noreply.github.com>
2018-08-03 09:34:56 +0600

    Add empty class LogRepository

---
+++ OnlineJudge/Repository/LogRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineJudge.Repository {
+    public class LogRepository {
+
+        // returns all logs in reverse chronological order
+        public void GetAllLogs(){
+            throw new NotImplementedException();
+        }
+
+        // returns all logs in reverse chronological order
+        public void GetLogs(int from, int to){
+            // use skip() and take to select the range specified 
+            throw new NotImplementedException();
+        }
+    }
+}
