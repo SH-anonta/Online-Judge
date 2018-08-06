47d6b4dad7a06a0acf2ea3fec88c0595c24cb741
Anonta <SH-anonta@users.noreply.github.com>
2018-08-06 11:56:07 +0600

    Add new field to SubmisisonResponseModel

--- OnlineJudge/ResponseModels/SubmissionResponses.cs
+++ OnlineJudge/ResponseModels/SubmissionResponses.cs
@@ -9,6 +9,7 @@ namespace OnlineJudge.ResponseModels {
         public string ProblemTitle { set; get; }
         public int ProblemId { set; get; }
 
+        public string ProgrammingLanguage { set; get; }
         public string Status { set; get; }
         public string UserName { set; get; }
         public int UserId { set; get; }
@@ -19,13 +20,14 @@ namespace OnlineJudge.ResponseModels {
         public DateTime SubmissionDate { set; get; }
 
         public double RunningTime {set; get;}
-        public double PeakMemmoryUsage {set; get;}
+        public double PeakMemoryUsage {set; get;}
 
         public SubmissionResponseData(Submission submission){
             this.ProblemTitle = submission.Problem.Title;
             this.ProblemId = submission.Problem.Id;
 
             this.Status = submission.Status.Name;
+            this.ProgrammingLanguage  = submission.ProgrammingLanguage.Name;
 
             this.UserName = submission.Submitter.UserName;
             this.UserId = submission.Submitter.Id;
@@ -35,7 +37,7 @@ namespace OnlineJudge.ResponseModels {
             this.SubmissionDate = submission.SubmissionDate;
             
             this.RunningTime = submission.RunningTime;
-            this.PeakMemmoryUsage = submission.PeakMemmoryUsage;
+            this.PeakMemoryUsage = submission.PeakMemmoryUsage;
         }
 
         public static List<SubmissionResponseData> MapTo(IEnumerable<Submission> subs){
