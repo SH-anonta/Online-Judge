0957eb9d6cb35c1817dee71c5ded579767231273
Manosh Talukder <manosh.office015@gmail.com>
2018-08-28 23:10:11 +0600

    Modified Authorization attribute for HomeController

--- OnlineJudge/Controllers/HomeController.cs
+++ OnlineJudge/Controllers/HomeController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace OnlineJudge.Controllers {
     public class HomeController : Controller {
-        [Authorize]
+        [System.Web.Http.Authorize]        
         public ActionResult Index() {
             return View();
         }
