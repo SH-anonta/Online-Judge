01e5bede487cdec774be79deccbe824ed0b04b0b
Manosh Talukder <manosh.office015@gmail.com>
2018-09-03 23:09:19 +0600

    Added model for keeping System Log for admin.

---
+++ OnlineJudge/Models/SystemLog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineJudge.Models
+{
+    public class SystemLog
+    {
+        public int Id { get; set; }
+        public DateTime Date{ get; set; }
+        public int Thread { get; set; }
+        public string Level { get; set; }
+        public string Logger { get; set; }
+        public string Message { get; set; }
+        public string Exception { get; set; }
+    }
+}
